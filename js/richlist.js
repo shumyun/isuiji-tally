fda83e711104d991b146f6393a67f81d54dcdb40
shumyun <shumyun@gmail.com>
2012-02-20 23:13:32 +0800

    updating

--- js/richlist.js
+++ js/richlist.js
@@ -9,14 +9,14 @@
 jQuery.noConflict();
 
 jQuery(document).ready(function($) {
-	//
-	ac_ajax = $('<div class="p_pop" style="position: absolute; z-index:301;">1</div>')
+	// z-index:301;
+	ac_ajax = $('<div class="p_pop" style="position: absolute; border-top: none;">1</div>')
 	.appendTo("body")
 	.position({
 		my: "left top",
 		at: "left bottom",
 		of: $("#li_popmenu"),
-		offset: "0 0"
+		offset: "0 2"
 	}).hide();
 	
 	$("#li_popmenu").toggle(
@@ -26,5 +26,10 @@ jQuery(document).ready(function($) {
 	
 	$("#li_popmenu").click(function() {
 		$("#li_popmenu").toggleClass("ac_showm ac_hidem");
+		if($("#li_popmenu").hasClass("ac_hidem")) {
+			$("#a_time").attr("style", "border-bottom-color: #CDCDCD;");
+		} else {
+			$("#a_time").attr("style", "border-bottom-color: #fff;");
+		}
 	});
 });
