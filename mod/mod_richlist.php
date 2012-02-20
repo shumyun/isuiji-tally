192cd18174799d2535dbc6a763564dae39ae1022
shumyun <shumyun@gmail.com>
2012-02-20 12:23:26 +0800

    少上传一个文件

---
+++ mod/mod_richlist.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *    account v0.1.0
+ *    Plug-in for Discuz!
+ *    Last Updated: 2012-02-18
+ *    Author: shumyun
+ *    Copyright (C) 2011 - forever jiashe.net Inc
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+
+
+?>
