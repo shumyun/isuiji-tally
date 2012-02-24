ba5e812ea70f1f2856b31683be4c7417ed8f882e
shumyun <shumyun@gmail.com>
2012-02-24 11:25:49 +0800

    updating

--- js/richlist.js
+++ js/richlist.js
@@ -13,10 +13,9 @@ jQuery.noConflict();
  * 时间选择控件
  */
 jQuery(document).ready(function($) {
-	// z-index:301;
-	ac_ajax = $('<div class="p_pop ac_li" style="position: absolute; border-top: none;">\
+	var sel_time1 = $('<div class="p_pop ac_li" style="position: absolute; border-top: none;">\
 					<ul>\
-						<li>本月</li><li>上一个月</li><li>本年</li><li>按月查询</li><li>按时间段查询</li>\
+						<li>最近一个月</li><li>上一个月</li><li>最近一年</li><li>按月份查询</li>\
 					</ul></div>')
 	.appendTo("body")
 	.position({
@@ -27,20 +26,43 @@ jQuery(document).ready(function($) {
 	}).hide();
 	
 	$(document).click(function(e){
-		if( !ac_ajax.is(":hidden") && e.target.id != "li_popmenu") {
+		if( !sel_time1.is(":hidden") && e.target.id != "li_popmenu" && e.target.id != "a_popmenu") {
 			$("#li_popmenu").click();
 		}
 	});
 	
 	$("#li_popmenu").click(function() {
 		$("#li_popmenu").toggleClass("ac_showm ac_hidem");
-		if(ac_ajax.is(":hidden")){
+		if(sel_time1.is(":hidden")){
 			$("#a_time").attr("style", "border-bottom-color: #CDCDCD;");
-			ac_ajax.show();
+			sel_time1.show();
 		} else {
 			$("#a_time").attr("style", "border-bottom-color: #fff;");
-			ac_ajax.hide();
+			sel_time1.hide();
 		}
 	});
+
+	var btn_name = $('<div class="ac_sel ac_hidem" style="position: absolute; z-index:101;"><a>账单分类</a></div>')
+	.appendTo("body")
+	.position({
+		my: "center center",
+		at: "center center",
+		of: $("#sel_name")
+	}).hide();
 	
+	var sel_name = $('<div class="p_pop ac_li" style="position: absolute;">\
+			<ul>\
+				<li>最近一个月</li><li>上一个月</li><li>最近一年</li><li>按月份查询</li>\
+			</ul></div>')
+	.appendTo("body").hide();
+	
+	$("#sel_name").click(function(){
+		btn_name.show();
+		sel_name.position({
+			my: "left top",
+			at: "left bottom",
+			of: btn_name,
+			offset: "0 -1"
+		}).show();
+	});
 });
