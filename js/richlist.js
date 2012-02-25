b8da19e2c8fc78c3031146176c3f6e01e43276ac
shumyun <shumyun@gmail.com>
2012-02-25 18:46:18 +0800

    updating

--- js/richlist.js
+++ js/richlist.js
@@ -13,51 +13,42 @@ jQuery.noConflict();
  * 时间选择控件
  */
 jQuery(document).ready(function($) {
-	$('#pop_time1').position({
+	var pop_time1 = $('<div class="p_pop ac_li"><ul><li>最近一个月</li><li>上一个月</li><li>最近一年</li><li>按月份查询</li></ul></div>')
+	.appendTo("body")
+	.position({
 		my: "left top",
 		at: "left bottom",
 		of: $("#li_popmenu"),
 		offset: "0 2"
-	});
+	}).hide();
 	
 	$(document).click(function(e){
-		if( !$('#pop_time1').is(":hidden") && e.target.id != "li_popmenu" && e.target.id != "a_popmenu") {
+		if( !pop_time1.is(":hidden") && e.target.id != "li_popmenu" && e.target.id != "a_popmenu") {
 			$("#li_popmenu").click();
 		}
 	});
 	
 	$("#li_popmenu").click(function() {
-		$("#li_popmenu").toggleClass("ac_showm ac_hidem");
-		if($('#pop_time1').is(":hidden")){
+		$("#li_popmenu").toggleClass("ac_showm li_hidem");
+		if(pop_time1.is(":hidden")){
 			$("#a_time").attr("style", "border-bottom-color: #CDCDCD;");
-			$('#pop_time1').show();
+			pop_time1.show();
 		} else {
 			$("#a_time").attr("style", "border-bottom-color: #fff;");
-			$('#pop_time1').hide();
+			pop_time1.hide();
 		}
 	});
-
-	var btn_name = $('<div class="ac_sel ac_hidem" style="position: absolute; z-index:101;"><a>账单分类</a></div>')
-	.appendTo("body")
-	.position({
-		my: "center center",
-		at: "center center",
-		of: $("#sel_name")
-	}).hide();
 	
-	var sel_name = $('<div class="p_pop ac_li" style="position: absolute;">\
-			<ul>\
-				<li>最近一个月</li><li>上一个月</li><li>最近一年</li><li>按月份查询</li>\
-			</ul></div>')
-	.appendTo("body").hide();
-	
-	$("#sel_name").click(function(){
-		btn_name.show();
-		sel_name.position({
-			my: "left top",
-			at: "left bottom",
-			of: btn_name,
-			offset: "0 -1"
-		}).show();
+	$("#sel_shown").click(function(){
+		var pos = $("#sel_shown").position();
+		$("#sel_hiden").attr("style", "display: block; left:"+pos.left+"px; top:"+pos.top+"px;");
+		var tmp = pos.top + 22;
+		$("#ac_popn").attr("style", "display: block; left:"+pos.left+"px; top:"+tmp+"px;");
 	});
+	
+	$("#sel_hiden").click(function(){
+		$("#sel_hiden").attr("style", "display: none");
+		$("#ac_popn").attr("style", "display: none");
+	});
+	
 });
