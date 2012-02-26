bbe194dbd14d36d2635a074e630d9ee987aef27f
shumyun <shumyun@gmail.com>
2012-02-26 21:49:58 +0800

    updating

--- js/richlist.js
+++ js/richlist.js
@@ -1,7 +1,7 @@
 /**
  *    account v0.1.0
  *    Plug-in for Discuz!
- *    Last Updated: 2012-02-23
+ *    Last Updated: 2012-02-26
  *    Author: shumyun
  *    Copyright (C) 2011 - forever jiashe.net Inc
  */
@@ -39,16 +39,74 @@ jQuery(document).ready(function($) {
 		}
 	});
 	
+	var popn_pos;
 	$("#sel_shown").click(function(){
-		var pos = $("#sel_shown").position();
-		$("#sel_hiden").attr("style", "display: block; left:"+pos.left+"px; top:"+pos.top+"px;");
-		var tmp = pos.top + 22;
-		$("#ac_popn").attr("style", "display: block; left:"+pos.left+"px; top:"+tmp+"px;");
+		popn_pos = $("#sel_shown").position();
+		$("#sel_hiden").attr("style", "display: block; left:"+popn_pos.left+"px; top:"+popn_pos.top+"px;");
+		popn_pos.top = popn_pos.top + 22;
+		$("#ac_popn").attr("style", "display: block; left:"+popn_pos.left+"px; top:"+popn_pos.top+"px;");
 	});
 	
 	$("#sel_hiden").click(function(){
 		$("#sel_hiden").attr("style", "display: none");
 		$("#ac_popn").attr("style", "display: none");
+	});
+	
+
+	$("#ul_popn > li").each(function(){
+		$(this).mouseenter(function() {
+			var a_clsid = $(this).attr("a_clsid");
+			var a_hclsid = parseInt(a_clsid) + 3;
+			$(this).attr("a_clsid", a_hclsid);
+			if($(this).hasClass("selimg_arrow")) {
+				$(this).toggleClass("selimg_harrow");
+				var divid = $(this).attr("div_id");
+				var pos = $(this).position();
+				pos.top = parseFloat(popn_pos.top) + parseFloat(pos.top);
+				pos.left = $("#ac_popn").width() + 5 + popn_pos.left;
+				$("#"+divid).attr("style", "display: block; left:"+pos.left+"px; top:"+pos.top+"px;");
+			}
+			$("a", this).toggleClass("ac_ahover selimg_"+a_clsid+" selimg_"+a_hclsid);
+		});
+	
+		$(this).mouseleave(function() {
+			var a_clsid = $(this).attr("a_clsid");
+			var a_hclsid = parseInt(a_clsid) - 3;
+			$(this).attr("a_clsid", a_hclsid);
+			if($(this).hasClass("selimg_arrow")) {
+				$(this).toggleClass("selimg_harrow");
+				var divid = $(this).attr("div_id");
+				$("#"+divid).mouseenter();
+				/*
+				$("#"+divid).attr("style", "display: none");*/
+			}
+			$("a", this).toggleClass("ac_ahover selimg_"+a_clsid+" selimg_"+a_hclsid);
+		});
+	});
+	
+	$("[popn='div']").each(function(){
+		$(this).mouseenter(function() {
+		});
+
+		$(this).mouseleave(function() {
+			$(this).attr("style", "display: none");
+		});
+	});
+	
+	$("[popn='div_ul'] > li").each(function(){
+		$(this).mouseenter(function() {
+			var a_clsid = $(this).attr("a_clsid");
+			var a_hclsid = parseInt(a_clsid) + 3;
+			$(this).attr("a_clsid", a_hclsid);
+			$("a", this).toggleClass("ac_ahover selimg_"+a_clsid+" selimg_"+a_hclsid);
+		});
+
+		$(this).mouseleave(function() {
+			var a_clsid = $(this).attr("a_clsid");
+			var a_hclsid = parseInt(a_clsid) - 3;
+			$(this).attr("a_clsid", a_hclsid);
+			$("a", this).toggleClass("ac_ahover selimg_"+a_clsid+" selimg_"+a_hclsid);
+		});
 	});
 	
 });
--- mod/mod_richlist.php
+++ mod/mod_richlist.php
@@ -3,7 +3,7 @@
 /**
  *    account v0.1.0
  *    Plug-in for Discuz!
- *    Last Updated: 2012-02-18
+ *    Last Updated: 2012-02-26
  *    Author: shumyun
  *    Copyright (C) 2011 - forever jiashe.net Inc
  */
@@ -12,6 +12,46 @@ if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 
+$arr = array(
+		0 => 'paytype',
+		1 => 'earntype'
+		);
 
+$account->run_radata($_G['uid'], $arr);
+
+$type = $account->account_config['paytype'];
+
+
+$div_i = 0;
+$div_arr = array();
+$ac_ndiv = '<div id="ac_popn" class="ac_pop">';
+$ac_ndiv .= '<ul id="ul_popn">';
+foreach($type as $data) {
+	foreach($data as $key => $val) {
+		if($val == '.'){
+			$ac_ndiv .= '<li a_clsid="0"><a class="selimg_empty selimg_0">'.$key.'</a></li>';
+		} else if(is_array($val)) {
+			$div_arr[$div_i] = $val;
+			$ac_ndiv .= '<li class="selimg_arrow" a_clsid="0" div_id="popn_'.$div_i.'"><a class="selimg_empty selimg_0">'.$key.'</a></li>';
+			$div_i++;
+		}
+	}
+}
+$ac_ndiv .= '</ul>';
+$ac_ndiv .= '</div>';
+
+foreach($div_arr as $key => $data) {
+	$ac_ndiv .= '<div id="popn_'.$key.'" class="ac_pop" popn="div">';
+	$ac_ndiv .= '<ul popn="div_ul">';
+	foreach($data as $val) {
+		foreach($val as $name => $data1){
+			if($data1 == '.') {
+				$ac_ndiv .= '<li a_clsid="0"><a class="selimg_empty selimg_0">'.$name.'</a></li>';
+			}
+		}
+	}
+	$ac_ndiv .= '</ul>';
+	$ac_ndiv .= '</div>';
+}
 
 ?>
