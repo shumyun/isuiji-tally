90ffc92d026395348c2167ec81b6359210d6ef54
shumyun <shumyun@gmail.com>
2013-01-05 19:41:39 +0800

    updating

--- mod/mod_richlist.php
+++ mod/mod_richlist.php
@@ -42,8 +42,7 @@ foreach($type as $data) {
 }
 $ac_pdiv .= '</ul>';
 $ac_pdiv .=	'<a id="a_pay" class="ac_h selimg_empty selimg_0" a_ulid="ul_pay" a_clsid="0">全部</a>
-							<br />
-							<button id="btn_pay" class="pn pnc"><strong>确定</strong></button></div>';
+				<button id="btn_pay" class="pn pnc" style="margin: 8px 0px 6px 20px;"><strong>确定</strong></button></div>';
 //支出子菜单
 foreach($div_arr as $key => $data) {
 	$ac_pdiv .= '<div id="pop_p'.$key.'" class="ac_pop" div_ulid="ul_p'.$key.'">';
@@ -82,8 +81,7 @@ foreach($type as $data) {
 }
 $ac_ediv .= '</ul>';
 $ac_ediv .= '<a id="a_earn" class="ac_h selimg_empty selimg_0" a_ulid="ul_earn" a_clsid="0">全部</a>
-							<br />
-							<button id="btn_earn" class="pn pnc"><strong>确定</strong></button></div>';
+				<button id="btn_earn" class="pn pnc" style="margin: 8px 0px 6px 20px;"><strong>确定</strong></button></div>';
 //收入子菜单
 foreach($div_arr as $key => $data) {
 	$ac_ediv .= '<div id="pop_e'.$key.'" class="ac_pop" div_ulid="ul_e'.$key.'">';
@@ -113,8 +111,7 @@ foreach($type as $data) {
 }
 $ac_bdiv .= '</ul>';
 $ac_bdiv .= '<a id="a_belong" class="ac_h selimg_empty selimg_0" a_ulid="ul_belong" a_clsid="0">全部</a>
-							<br />
-							<button id="btn_belong" class="pn pnc"><strong>确定</strong></button></div>';
+				<button id="btn_belong" class="pn pnc" style="margin: 8px 0px 6px 20px;"><strong>确定</strong></button></div>';
 
 /*
  * 借贷归属菜单
@@ -130,8 +127,7 @@ foreach($type as $data) {
 }
 $ac_ldiv .= '</ul>';
 $ac_ldiv .= '<a id="a_loandebt" class="ac_h selimg_empty selimg_0" a_ulid="ul_loandebt" a_clsid="0">全部</a>
-							<br />
-							<button id="btn_loandebt" class="pn pnc"><strong>确定</strong></button></div>';
+				<button id="btn_loandebt" class="pn pnc" style="margin: 8px 0px 6px 20px;"><strong>确定</strong></button></div>';
 
 $richlist_y = date('Y', $_G['timestamp']);
 $richlist_m = date('n', $_G['timestamp']);
