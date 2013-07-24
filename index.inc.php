b248c803182720ac3dc6705f0892ca8a56e08a2d
shumyun <shumyun@gmail.com>
2013-07-24 22:54:31 +0800

    updating

--- index.inc.php
+++ index.inc.php
@@ -3,7 +3,7 @@
 /**
  *    account v0.1.0
  *    Plug-in for Discuz!
- *    Last Updated: 2013-02-04
+ *    Last Updated: 2013-07-24
  *    Author: shumyun
  *    Copyright (C) 2011 - forever isuiji.com Inc
  */
@@ -31,12 +31,17 @@ global $account;
 
 $acc_date = dgmdate($_G['timestamp'], 'd');
 
-$acc_mod = in_array($_GET['mod'], array('richadd', 'richlist', 'richbudget', 'richcount', 'richset')) ? trim($_GET['mod']) : $defaultop;
+$acc_mod = in_array($_GET['mod'], array('richadd', 'richlist', 'budget', 'richcount', 'richset')) ? trim($_GET['mod']) : $defaultop;
 
 switch ($acc_mod) {
 	case 'richlist':
 		require_once 'mod/mod_richlist.php';
 		include template('account:richlist');
+		break;
+		
+	case 'budget':
+		require_once 'mod/mod_budget.php';
+		include template('account:budget');
 		break;
 		
 	default:
---
+++ mod/mod_budget.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *    account v0.1.0
+ *    Plug-in for Discuz!
+ *    Last Updated: 2013-07-24
+ *    Author: shumyun
+ *    Copyright (C) 2011 - forever isuiji.com Inc
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+?>
