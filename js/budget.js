89abd8467bd2fb15f8c7b8bf141d6d48960bbcc7
shumyun <shumyun@gmail.com>
2013-08-01 23:29:30 +0800

    updating

---
+++ js/budget.js
@@ -0,0 +1,14 @@
+/**
+ *    account v0.1.0
+ *    Plug-in for Discuz!
+ *    Last Updated: 2013-08-01
+ *    Author: shumyun
+ *    Copyright (C) 2011 - forever isuiji.com Inc
+ */
+
+jQuery(document).ready(function($) {
+	$("#prev_year").click(function(){
+		$("#tally_year").val($("#tally_year").val()+1);
+	});
+	
+});
