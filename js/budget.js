10afc7ab1f6a78077782e6856a8ec426156492b4
shumyun <shumyun@gmail.com>
2013-08-02 22:41:20 +0800

    updating

--- js/budget.js
+++ js/budget.js
@@ -6,19 +6,21 @@
  *    Copyright (C) 2011 - forever isuiji.com Inc
  */
 
-var func_prevm = function(imonth, sel=0){
+var func_prevm = function(imonth, sel=0) {
 	if(imonth <= 0) {
 		if(sel) jQuery("#tally_year").val(parseInt(jQuery("#tally_year").val())-1);
 		return imonth+12;
 	} else return imonth;
-}
+};
 
-var func_nextm = function(imonth, sel=0){
+var func_nextm = function(imonth, sel=0) {
 	if(imonth > 12) {
 		if(sel) jQuery("#tally_year").val(parseInt(jQuery("#tally_year").val())+1);
 		return imonth-12;
 	} else return imonth;
-}
+};
+
+var g_imonth = 0;
 
 jQuery(document).ready(function($) {
 	$("#prev_year").click(function(){
@@ -29,26 +31,39 @@ jQuery(document).ready(function($) {
 		$("#tally_year").val(parseInt($("#tally_year").val())+1);
 	});
 	
-	var imonth = parseInt($("#a_m3").html());
-	$("#a_m1").html(func_prevm(imonth-2)+"月");
-	$("#a_m2").html(func_prevm(imonth-1)+"月");
-	$("#a_m4").html(func_nextm(imonth+1)+"月");
-	$("#a_m5").html(func_nextm(imonth+2)+"月");
+	g_imonth = parseInt($("#a_m3").html());
+	$("#a_m1").html(func_prevm(g_imonth-2)+"月");
+	$("#a_m2").html(func_prevm(g_imonth-1)+"月");
+	$("#a_m4").html(func_nextm(g_imonth+1)+"月");
+	$("#a_m5").html(func_nextm(g_imonth+2)+"月");
 	
 	$("#prev_month").click(function(){
-		$("#a_m1").html(func_prevm(parseInt($("#a_m1").html())-1)+"月");
-		$("#a_m2").html(func_prevm(parseInt($("#a_m2").html())-1)+"月");
-		$("#a_m3").html(func_prevm(parseInt($("#a_m3").html())-1, 1)+"月");
-		$("#a_m4").html(func_prevm(parseInt($("#a_m4").html())-1)+"月");
-		$("#a_m5").html(func_prevm(parseInt($("#a_m5").html())-1)+"月");
+		$("li.acb_mli > a[class!='acb_msel']").each(function(){
+			$(this).html(func_prevm(parseInt($(this).html())-1)+"月");
+		});
+		g_imonth = func_prevm(g_imonth-1, 1);
+		$("a.acb_msel").html(g_imonth+"月");
 	});
 	
 	$("#next_month").click(function(){
-		$("#a_m1").html(func_nextm(parseInt($("#a_m1").html())+1)+"月");
-		$("#a_m2").html(func_nextm(parseInt($("#a_m2").html())+1)+"月");
-		$("#a_m3").html(func_nextm(parseInt($("#a_m3").html())+1, 1)+"月");
-		$("#a_m4").html(func_nextm(parseInt($("#a_m4").html())+1)+"月");
-		$("#a_m5").html(func_nextm(parseInt($("#a_m5").html())+1)+"月");
+		$("li.acb_mli > a[class!='acb_msel']").each(function(){
+			$(this).html(func_nextm(parseInt($(this).html())+1)+"月");
+		});
+		g_imonth = func_nextm(g_imonth+1, 1);
+		$("a.acb_msel").html(g_imonth+"月");
+	});
+
+	$("li.acb_mli > a").each(function(){
+		$(this).click(function(){
+			if(!$(this).hasClass("acb_msel")) {
+				$("a.acb_msel").removeClass("acb_msel");
+				$(this).addClass("acb_msel");
+				var tmp = parseInt($(this).html()) - g_imonth;
+				if(tmp < -5) $("#next_year").click();
+				else if(tmp > 5) $("#prev_year").click();
+				g_imonth += tmp;
+			}
+		});
 	});
 	
 });
