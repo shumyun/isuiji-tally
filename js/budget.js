ad328c084928d16ab27322e1c8a12ed5918ed232
shumyun <shumyun@gmail.com>
2013-08-02 17:37:48 +0800

    updating

--- js/budget.js
+++ js/budget.js
@@ -1,14 +1,54 @@
 /**
  *    account v0.1.0
  *    Plug-in for Discuz!
- *    Last Updated: 2013-08-01
+ *    Last Updated: 2013-08-02
  *    Author: shumyun
  *    Copyright (C) 2011 - forever isuiji.com Inc
  */
 
+var func_prevm = function(imonth, sel=0){
+	if(imonth <= 0) {
+		if(sel) jQuery("#tally_year").val(parseInt(jQuery("#tally_year").val())-1);
+		return imonth+12;
+	} else return imonth;
+}
+
+var func_nextm = function(imonth, sel=0){
+	if(imonth > 12) {
+		if(sel) jQuery("#tally_year").val(parseInt(jQuery("#tally_year").val())+1);
+		return imonth-12;
+	} else return imonth;
+}
+
 jQuery(document).ready(function($) {
 	$("#prev_year").click(function(){
-		$("#tally_year").val($("#tally_year").val()+1);
+		$("#tally_year").val(parseInt($("#tally_year").val())-1);
+	});
+	
+	$("#next_year").click(function(){
+		$("#tally_year").val(parseInt($("#tally_year").val())+1);
+	});
+	
+	var imonth = parseInt($("#a_m3").html());
+	$("#a_m1").html(func_prevm(imonth-2)+"月");
+	$("#a_m2").html(func_prevm(imonth-1)+"月");
+	$("#a_m4").html(func_nextm(imonth+1)+"月");
+	$("#a_m5").html(func_nextm(imonth+2)+"月");
+	
+	$("#prev_month").click(function(){
+		$("#a_m1").html(func_prevm(parseInt($("#a_m1").html())-1)+"月");
+		$("#a_m2").html(func_prevm(parseInt($("#a_m2").html())-1)+"月");
+		$("#a_m3").html(func_prevm(parseInt($("#a_m3").html())-1, 1)+"月");
+		$("#a_m4").html(func_prevm(parseInt($("#a_m4").html())-1)+"月");
+		$("#a_m5").html(func_prevm(parseInt($("#a_m5").html())-1)+"月");
+	});
+	
+	$("#next_month").click(function(){
+		$("#a_m1").html(func_nextm(parseInt($("#a_m1").html())+1)+"月");
+		$("#a_m2").html(func_nextm(parseInt($("#a_m2").html())+1)+"月");
+		$("#a_m3").html(func_nextm(parseInt($("#a_m3").html())+1, 1)+"月");
+		$("#a_m4").html(func_nextm(parseInt($("#a_m4").html())+1)+"月");
+		$("#a_m5").html(func_nextm(parseInt($("#a_m5").html())+1)+"月");
 	});
 	
 });
--- mod/mod_budget.php
+++ mod/mod_budget.php
@@ -3,7 +3,7 @@
 /**
  *    account v0.1.0
  *    Plug-in for Discuz!
- *    Last Updated: 2013-07-24
+ *    Last Updated: 2013-08-02
  *    Author: shumyun
  *    Copyright (C) 2011 - forever isuiji.com Inc
  */
@@ -11,5 +11,8 @@
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
+
+$richbudget_y = date('Y', $_G['timestamp']);
+$richbudget_m = date('n', $_G['timestamp']);
 
 ?>
